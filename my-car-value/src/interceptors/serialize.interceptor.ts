7649b8ab2ee249bc8da30e001dbfd2f72c4ac943
Alexandra Astileanu <alexandra.astileanu@pitechplus.com>
2024-01-20 11:16:31 +0200

    Refactor the interceptor to use any dto set in the controller

--- my-car-value/src/interceptors/serialize.interceptor.ts
+++ my-car-value/src/interceptors/serialize.interceptor.ts
@@ -2,19 +2,14 @@ import { UseInterceptors, NestInterceptor, ExecutionContext, CallHandler } from 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
-import { UserDto } from '../users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
-    // run something before the request is handled 
-    // by the request handler 
-    console.log('I am running before the handler', context);
+  constructor(private dto: any) {}
 
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     return handler.handle().pipe(
       map((data: any) => {
-        // run smth before the response is sent out
-        console.log('I am running before response is sent out', data);
-        return plainToClass(UserDto, data, {
+        return plainToClass(this.dto, data, {
           excludeExtraneousValues: true,
         })
       })
