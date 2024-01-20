c723576f6384c5bf01c335ba6fd2115285f759a8
Alexandra Astileanu <alexandra.astileanu@pitechplus.com>
2024-01-20 17:57:44 +0200

    Sign out and fix for displaying first user if session cookie is null

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -39,6 +39,12 @@ export class UsersController {
     return user;
   }
 
+  @Post('/signout')
+  signout(@Session() session: any) {
+    session.userId = null; 
+   
+  }
+
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
