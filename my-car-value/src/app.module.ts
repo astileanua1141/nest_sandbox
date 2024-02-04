672a85c9bc2e771aa49a6c8cc2a656fe023af337
Alexandra Astileanu <alexandra.astileanu@pitechplus.com>
2024-02-04 18:59:26 +0200

    Move the pipe and cookie session middleware in app module

--- my-car-value/src/app.module.ts
+++ my-car-value/src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
@@ -6,6 +7,7 @@ import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+const cookieSession = require('cookie-session'); // old way to import, because of tsconfig
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -15,6 +17,21 @@ import { Report } from './reports/report.entity';
     synchronize: true
   }), UsersModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      })
+    }],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieSession({
+      keys: ['asdeyfgd75nbs'] // this string is used to encypt the information inside the cookie
+    }))
+      .forRoutes('*');
+
+  }
+}
--- my-car-value/src/main.ts
+++ my-car-value/src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-const cookieSession = require('cookie-session'); // old way to import, because of tsconfig
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['asdeyfgd75nbs'] // this string is used to encypt the information inside the cookie
-  }))
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
-  )
   await app.listen(3000);
 }
 bootstrap();
